24f2ace4b0df5e1f85bf8d6bf7da1363e0c310f7
Michael (PolyMj) <ballarmj@sunypoly.edu>
2024-01-30 22:52:00 -0500

    Kind of a mess, but I think room generation is mostly good*

--- Room.py
+++ Room.py
@@ -2,42 +2,54 @@ import pygame
 import random
 
 # Currently only two types of buildings: Building & Emptys
-BUILDING_TYPES = 2
-TILE_COUNT = 16
-LOOT_VALUE = 100
+BUILDING_TYPES = 5
+TILE_COUNT = 32
+LOOT_VALUE = TILE_COUNT*6
 
 # These may become class parameters later on
-TILE_HEIGHT = 96
-TILE_WIDTH = 800
-BUILDING_MAX_HEIGHT = TILE_HEIGHT * 2
+TILE_HEIGHT = 64
+TILE_WIDTH = 1000
 BUILDING_WIDTH = 192
+BUILDING_MAX_HEIGHT = TILE_HEIGHT - 4
 BUILDING_PORCH_WIDTH = 64
 
-BORDER_SIZE = 200 # for testing
 
 class Room():
-	def __init__(self):
+	def __init__(self, total_loot = LOOT_VALUE):
 		self.tile_list: list[Tile] = []
-		remaining_loot = LOOT_VALUE
+		remaining_loot = total_loot
 		
-		# Generate tiles
+		# Generate tiles white distributing loot
 		remaining_tiles = TILE_COUNT - 1
 		while remaining_tiles > 0:
 			tile_loot = 0
-			if random.getrandbits(2):
-				tile_loot = remaining_loot // remaining_tiles
-				remaining_loot -= tile_loot
+			# Get loot for the tile
+			bitsX2 = random.getrandbits(4)
+			if bitsX2 & 0b0011: 
+				l = remaining_loot // remaining_tiles
+				remaining_loot -= l
+				tile_loot += l
+			if not bitsX2 & 0b1110: 
+				l = min(remaining_loot, 2 * remaining_loot // remaining_tiles)
+				remaining_loot -= l
+				tile_loot += l
+
+			# Create tile
 			self.tile_list.append(Tile(tile_loot))
 			remaining_tiles -= 1
+		
 		self.tile_list.append(Tile(remaining_loot))
 
+		# self.updateImage()
 
-	def getFullImage(self) -> pygame.Surface:
-		image = pygame.Surface((TILE_WIDTH + BORDER_SIZE * 2, TILE_HEIGHT * TILE_COUNT + BORDER_SIZE * 2))
+
+	def updateImage(self) -> pygame.Surface:
+		image = pygame.Surface((TILE_WIDTH, TILE_HEIGHT * TILE_COUNT))
 		image.fill((80, 80, 80, 255))
 		for i in range(0, TILE_COUNT):
-			image.blit(self.tile_list[i].image, (BORDER_SIZE, TILE_HEIGHT * i + BORDER_SIZE))
-		return image
+			self.tile_list[i].updateImage()
+			image.blit(self.tile_list[i].image, (0, TILE_HEIGHT * i))
+		self.image = image
 
 	
 	def __str__(self):
@@ -53,34 +65,35 @@ class Room():
 class Tile():
 	def __init__(self, loot_value):
 		self.image = pygame.Surface((TILE_WIDTH, TILE_HEIGHT), pygame.SRCALPHA)
-		type_right = random.randint(0, BUILDING_TYPES - 1)
-		type_left = random.randint(0, BUILDING_TYPES - 1)
+		type_right = 1 #random.randint(0, BUILDING_TYPES - 1)
+		type_left = 1 #random.randint(0, BUILDING_TYPES - 1)
 		self.total_loot = loot_value
 		self.street_loot = 0
 
 		if (loot_value <= 0):
-			self.building_right = Building(type_right, 0)
-			self.building_left = Building(type_left, 0)
+			self.building_right = Building(type_right, False, 0)
+			self.building_left = Building(type_left, True, 0)
 		elif not random.getrandbits(3): # 1 in 8 chance to place all loot in the street
 			self.street_loot = loot_value
-			self.building_right = Building(type_right, 0)
-			self.building_left = Building(type_left, 0)
+			self.building_right = Building(type_right, False, 0)
+			self.building_left = Building(type_left, True, 0)
 		else:
 			bundle_1 = random.randint(0, loot_value)
 			bundle_2 = loot_value - bundle_1
 
-			self.building_right = Building(type_right, bundle_1)
-			self.building_left = Building(type_left, bundle_2)
+			self.building_right = Building(type_right, False, bundle_1)
+			self.building_left = Building(type_left, True, bundle_2)
+		# self.updateImage()
 
-		self.update()
-
-	def update(self):
+	def updateImage(self):
 		self.image.fill((0,0,0,0))
+		self.building_left.updateImage()
+		self.building_right.updateImage()
 		self.image.blit(self.building_left.image, (0,0))
 		self.image.blit(self.building_right.image, (TILE_WIDTH - BUILDING_WIDTH, 0))
 		# Draw street loot
 		if (self.street_loot):
-			pygame.draw.circle(self.image, (0,0,0), (TILE_WIDTH//2, TILE_HEIGHT//2), 10)
+			pygame.draw.circle(self.image, (0,0,0), (TILE_WIDTH//2, TILE_HEIGHT//2), 4*(self.street_loot**0.5))
 
 	def __str__(self):
 		return "%7s    %3d    %7s | T=%3d" % (self.building_left, self.street_loot, self.building_right, self.total_loot)
@@ -92,27 +105,43 @@ class Building():
 	for i in range(0, BUILDING_TYPES):
 		building.append(pygame.Surface((BUILDING_WIDTH, BUILDING_MAX_HEIGHT), pygame.SRCALPHA))
 	for i in range(1, BUILDING_TYPES):
-		building[i].fill((200, random.randint(0, 255), 15, 200))
+		building[i].fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
 
-	def __init__(self, type, loot_value):
+	# Contructor (style of house), (side of street), (value of loot it contains)
+	def __init__(self, type, is_on_left, loot_value):
 		self.valueToLoot(loot_value)
 		self.type = type
-		self.update()
+		self.faces_right = is_on_left
+		# self.updateImage()
 	
-	# Placeholder funciton
 	# Will eventually create loot objects and place them in random locations
-	def valueToLoot(self, total_value):
-		self.loot_value = total_value
+	def valueToLoot(self, loot_value):
+		self.loot_value = loot_value
 
-	def update(self):
-		self.image = pygame.Surface((BUILDING_MAX_HEIGHT, BUILDING_WIDTH), pygame.SRCALPHA)
-		self.image.blit(Building.building[self.type], (0,0))
-		# Draw loot
-		pygame.draw.circle(self.image, (0,0,0), ((BUILDING_WIDTH + BUILDING_PORCH_WIDTH // 2), BUILDING_MAX_HEIGHT - (TILE_HEIGHT // 2)), 100)
+	def updateImage(self):
+		self.image = pygame.Surface((BUILDING_WIDTH, BUILDING_MAX_HEIGHT), pygame.SRCALPHA)
+		# If there is a building to be drawn
+		if (self.type != 0):
+			self.image.blit(Building.building[self.type], (0,0))
+			# If the building need to face right, flip
+			if (self.faces_right):
+				pygame.transform.flip(self.image, True, False)
+		
+		self.drawLoot()
 		# Draw porch roof
-	
+
+	# Just draws a circle on the center of the porch for now
+	def drawLoot(self):
+		y = BUILDING_MAX_HEIGHT // 2
+		if (self.faces_right):
+			x = BUILDING_WIDTH - BUILDING_PORCH_WIDTH // 2
+		else:
+			x = BUILDING_PORCH_WIDTH // 2
+		pygame.draw.circle(self.image, (0,0,0), (x, y), 4*(self.loot_value**0.5))
+
+		
 	def __str__(self):
 		string = ""
-		string += '#' if self.type == 1 else ' '
+		string += '#' if self.type else ' '
 		string += str(self.loot_value)
 		return string
--- RoomDriver.py
+++ RoomDriver.py
@@ -6,23 +6,26 @@ import sys
 
 # Initial variables
 screen_width, screen_height = 1000, 720
-room_width, room_height = 3000, 3000
+room_width, room_height = 1000, 8000
 frame_rate = 60
 SPRITE_SCALE = 5
 
+FRAMES_AVG_OVER = 60
+REGENERATE_ROOM_RATE = 10
+
 room = Room.Room()
+room.updateImage()
+background_image = room.image
+background_rect = background_image.get_rect()
 
 print(room)
 
-background_image = room.getFullImage()
-background_rect = background_image.get_rect()
-
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Player Class Testing")
+pygame.display.set_caption("Room Generation Testing")
 
 # Making an instance of the Player and placing them in the center of the screen
-player = Player("Assets/doux.png", (24, 24), 24, screen_width // 2, screen_width // 2, SPRITE_SCALE)
+player = Player("Assets/doux.png", (24, 24), 24, screen_width // 2, screen_height // 2, SPRITE_SCALE)
 
 # Making a camera that is the size of the room
 camera = Camera(room_width, room_height, screen_width, screen_height)
@@ -35,31 +38,46 @@ all_sprites.add(player)
 clock = pygame.time.Clock()
 running = True
 
+f = 0
+r = 0
+ft = 0
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # Update calls for objects (aka: ticking)
-    player.update()
-    camera.update(player)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	
+	# Update calls for objects (aka: ticking)
+	player.update()
+	camera.update(player)
 
-    # Draw calls for objects (aka: rendering)
-    
-    screen.fill((0,0,0))
+	# Draw calls for objects (aka: rendering)
+	
+	screen.fill((0,0,0))
 
-    # Drawing the background
-    screen.blit(background_image, camera.apply(background_rect))
+	# Drawing the background
+	screen.blit(background_image, camera.apply(background_rect))
 
-    # Drawing all objects that we added to all_sprites
-    for sprite in all_sprites:
-        screen.blit(sprite.image, camera.apply(sprite))
+	# Drawing all objects that we added to all_sprites
+	for sprite in all_sprites:
+		screen.blit(sprite.image, camera.apply(sprite))
 
-    # Refresh (or else the old stuff stays)
-    pygame.display.flip()
+	# Refresh (or else the old stuff stays)
+	pygame.display.flip()
+	
+	r = (r + 1) % REGENERATE_ROOM_RATE
+	if not r:
+		room = Room.Room()
+		room.updateImage()
+		background_image = room.image
+		background_rect = background_image.get_rect()
+	
 
-    # Cap frame rate
-    ft = clock.tick(frame_rate)
+	f = (f + 1) % FRAMES_AVG_OVER
+	# Cap frame rate
+	ft += clock.tick(frame_rate)
+	if not f:
+		print("%3.2f (ms/frame)" % (ft / FRAMES_AVG_OVER))
+		ft = 0
 
 
 # Quit
