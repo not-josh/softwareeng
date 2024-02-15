eabec6bda1dbe52f1ea87e4551e3940d8d5c7485
Dylan Bartell <barteldf@sunypoly.edu>
2024-02-14 20:15:08 -0500

    Labeled player class entity creation

--- src/Player.py
+++ src/Player.py
@@ -4,9 +4,10 @@ import Inventory
 
 class Player(Entity.Entity, pygame.sprite.Sprite):
     def __init__(self, texture):
-        super().__init__(texture, (100,100), (400,400), 100, 5)
+        super().__init__(   texture,    (100,100),  (400,400),  100,    5)
+        #                   ^ img file  ^ size      ^start pos  ^health ^speed
         self.points = 0 #probably best to store points/money directly, rather than in inventory
         #self.inventory = Inventory.Inventory()
     
-    def move(self):
-        super().move()
+    #def move(self):
+    #    super().move()
