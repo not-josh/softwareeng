7e8044846d16ba1ab6495ed3a8dc7064aab591fb
Dylan Bartell <barteldf@sunypoly.edu>
2024-02-19 12:37:24 -0500

    Moved key movement functionality from entity to player

--- src/Player.py
+++ src/Player.py
@@ -9,5 +9,27 @@ class Player(Entity.Entity):# pygame.sprite.Sprite):
         self.points = 0 #probably best to store points/money directly, rather than in inventory
         self.inventory = Inventory.Inventory()
     
-    #def move(self):
-    #    super().move()
+    def move(self):
+        move = [0,0]
+        dir = "none"
+        if (pygame.key.get_pressed()[pygame.K_a]):
+            move[0] -= self.speed
+            dir = "left.png"
+            #self.surface = pygame.image.load(self.texture_folder + "left.png")
+        if (pygame.key.get_pressed()[pygame.K_d]):
+            move[0] += self.speed
+            dir = "right.png"
+            #new_direction = 'r'
+            #self.surface = pygame.image.load(self.texture_folder + "right.png")
+        if (pygame.key.get_pressed()[pygame.K_w]):
+            move[1] -= self.speed
+            dir = "up.png"
+            #self.surface = pygame.image.load(self.texture_folder + "up.png")
+        if (pygame.key.get_pressed()[pygame.K_s]):
+            move[1] += self.speed
+            dir = "down.png"
+            #self.surface = pygame.image.load(self.texture_folder + "down.png")
+        if (dir != "none"):
+            self.surface = pygame.transform.scale(pygame.image.load(self.texture_folder + dir),self.size)
+        self.rect.left += move[0]
+        self.rect.top += move[1]
